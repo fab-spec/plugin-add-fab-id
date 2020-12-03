f68dedc7a2fdf58b411a36cd7d101ae3f960e9da
Glen Maddern <glenmaddern@gmail.com>
2020-12-03 14:24:23 +0000

    omfg expect is now missing from the types wtaf

---
+++ tests/public.test.ts
@@ -0,0 +1,12 @@
+import shellac from 'shellac'
+
+describe('public installation', () => {
+  it('should be installable', async () => {
+    await shellac`
+    $ echo "Hello, world!"
+    stdout >> ${(echo) => {
+      expect(echo).toBe("Hello, world!")
+    }}
+  `
+  })
+})
