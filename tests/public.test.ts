a5372765cfacace478b1c70be2d45a806ad5a216
Glen Maddern <glenmaddern@gmail.com>
2020-12-03 14:35:48 +0000

    back in shellac country!

--- tests/public.test.ts
+++ tests/public.test.ts
@@ -1,13 +1,12 @@
-// import shellac from 'shellac'
+import shellac from 'shellac'
 
 describe('public installation', () => {
   it('should be installable', async () => {
-    expect(1+2).toBe(3)
-  //   await shellac`
-  //   $ echo "Hello, world!"
-  //   stdout >> ${(echo) => {
-  //     expect(echo).toBe("Hello, world!")
-  //   }}
-  // `
+    await shellac`
+      $ echo "Hello, world!"
+      stdout >> ${echo => {
+        expect(echo).toBe('Hello, world!')
+      }}
+    `
   })
 })
