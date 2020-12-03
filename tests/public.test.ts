79ee7b344e886429c7f9e5dd5af0064054e7df12
Glen Maddern <glenmaddern@gmail.com>
2020-12-03 14:34:07 +0000

    ok, it was being nested inside the FAB repo with a .tsconfig file further up that was messing things up

---
+++ tests/public.test.ts
@@ -0,0 +1,13 @@
+// import shellac from 'shellac'
+
+describe('public installation', () => {
+  it('should be installable', async () => {
+    expect(1+2).toBe(3)
+  //   await shellac`
+  //   $ echo "Hello, world!"
+  //   stdout >> ${(echo) => {
+  //     expect(echo).toBe("Hello, world!")
+  //   }}
+  // `
+  })
+})
