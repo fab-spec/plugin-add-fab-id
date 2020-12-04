89e5e448cf7f44b38762d765c2f7ae1b7da90e28
Glen Maddern <glenmaddern@gmail.com>
2020-12-04 11:39:45 +0000

    referencing the source file causes the build failure

--- index.test.ts
+++ index.test.ts
@@ -1,5 +1,6 @@
 import shellac from 'shellac'
 import { dir } from 'tmp-promise'
+import * as path from 'path'
 
 describe('@fab/plugin-add-fab-id', () => {
   it('should be installable from NPM', async () => {
@@ -9,13 +10,36 @@ describe('@fab/plugin-add-fab-id', () => {
       $ npm init -y
       $$ npx fab init --empty -y
 
-      $ yarn add @fab/plugin-add-fab-id
+      $$ yarn add @fab/plugin-add-fab-id
 
       $ echo '${JSON.stringify({
         plugins: {
           '@fab/plugin-add-fab-id': {}
         }
       })}' > fab.config.json5
+      
+      $$ yarn fab build
+      stdout >> ${build => expect(build).toMatch('Typecheck passed.')}
+      
+      $ ls -l
+      stdout >> ${files => expect(files).toMatch('fab.zip')}
+    `
+  }, 500000)
+
+  it('should be buildable locally', async () => {
+    const cwd = (await dir({ unsafeCleanup: true })).path
+
+    await shellac.in(cwd)`
+      $ npm init -y
+      $$ npx fab init --empty -y
+
+      $ echo '${JSON.stringify({
+        plugins: {
+          [path.join(__dirname, 'index.ts')]: {}
+        }
+      })}' > fab.config.json5
+      
+      $$ cat fab.config.json5
       
       $$ yarn fab build
       stdout >> ${build => expect(build).toMatch('Typecheck passed.')}
